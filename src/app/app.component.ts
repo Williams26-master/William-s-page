e78bd69b190f86c4075685bda3bb31ffedcbc247
Williams26-master <carvalho.william2666@gmail.com>
2023-12-15 02:52:27 -0300

    teste

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
